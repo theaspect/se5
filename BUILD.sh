cf882f3d71ad9c19e8e4b32f26b7317274deea28
Constantine Linnick <theaspect@gmail.com>
2019-03-16 06:30:49 +0000

    Add build scripts

---
+++ BUILD.sh
@@ -0,0 +1,3 @@
+echo Begin Build
+kotlinc -d out/aaa.jar -include-runtime src
+echo End Build
---
+++ TEST.sh
@@ -0,0 +1,9 @@
+./BUILD.sh
+
+echo
+echo No args
+./RUN.sh
+
+echo
+echo Many args
+./RUN.sh foo bar baz "1 2 3"
