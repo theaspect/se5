f3f9b8e1400e8ad79b444717179a647a70982334
Constantine Linnick <theaspect@gmail.com>
2019-03-16 04:47:34 +0000

    Functional refactoring

--- src/me/blzr/aaa/Main.kt
+++ src/me/blzr/aaa/Main.kt
@@ -3,9 +3,7 @@ package me.blzr.aaa
 fun main(args: Array<String>) {
     if (args.isEmpty()) {
         println("No Args!!!")
-    }else{
-        for (arg in args) {
-            println(arg)
-        }
+    } else {
+        args.forEach { println(it) }
     }
 }
