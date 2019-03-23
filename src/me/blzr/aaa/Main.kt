4da6a94a79a59470cdfc0fc74ac682d32a0de795
Constantine Linnick <theaspect@gmail.com>
2019-03-23 04:51:50 +0000

    Add exit codes

--- src/me/blzr/aaa/Main.kt
+++ src/me/blzr/aaa/Main.kt
@@ -3,7 +3,13 @@ package me.blzr.aaa
 fun main(args: Array<String>) {
     if (args.isEmpty()) {
         println("No Args!!!")
+        System.exit(0)
+    } else if (args.size == 1) {
+        System.exit(1)
+    } else if (args.size == 2) {
+        System.exit(2)
     } else {
         args.forEach { println(it) }
+        System.exit(100)
     }
 }
