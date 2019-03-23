96db8ee47815e3f882c6881b527f563dd3483882
Constantine Linnick <theaspect@gmail.com>
2019-03-23 05:34:38 +0000

    Add default callback value

--- src/me/blzr/aaa/Main.kt
+++ src/me/blzr/aaa/Main.kt
@@ -2,15 +2,19 @@ package me.blzr.aaa
 
 fun main(args: Array<String>) {
     val status = when (args.size) {
-        0 -> getStatus(0, { println("No Args!!!") })
-        1 -> getStatus(1, {})
-        2 -> getStatus(2, {})
-        else -> getStatus(100, { args.forEach { println(it) } })
+        0 -> getStatus(0) {
+            println("No Args!!!")
+        }
+        1 -> getStatus(1)
+        2 -> getStatus(2)
+        else -> getStatus(100) {
+            args.forEach { println(it) }
+        }
     }
     System.exit(status)
 }
 
-fun getStatus(status: Int, callback: () -> Unit): Int {
+fun getStatus(status: Int, callback: () -> Unit = {}): Int {
     callback()
     return status
 }
