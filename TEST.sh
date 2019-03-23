f221da8685899c51979c5786e45070eb2e60e741
Constantine Linnick <theaspect@gmail.com>
2019-03-23 03:45:51 +0000

    Add tests

--- TEST.sh
+++ TEST.sh
@@ -3,7 +3,39 @@
 echo
 echo No args
 ./RUN.sh
+RES=$?
+if [ $? -eq 0 ]; then
+    echo Test passed
+else
+    echo Test failed. Expected 0 Actual $RES
+fi
+
+echo
+echo 1 arg
+./RUN.sh foo
+RES=$?
+if [ $? -eq 1 ]; then
+    echo Test passed
+else
+    echo Test failed. Expected 1 Actual $RES
+fi
+
+echo
+echo 2 args
+./RUN.sh foo bar
+RES=$?
+if [ $? -eq 2 ]; then
+    echo Test passed
+else
+    echo Test failed. Expected 2 Actual $RES
+fi
 
 echo
 echo Many args
 ./RUN.sh foo bar baz "1 2 3"
+RES=$?
+if [ $? -eq 100 ]; then
+    echo Test passed
+else
+    echo Test failed. Expected 100 Actual $RES
+fi
