0b51864ad0d2b723d597150873100b313ff810fa
Constantine Linnick <theaspect@gmail.com>
2019-03-23 05:28:46 +0000

    Create getStatus with callback

--- src/me/blzr/aaa/Main.kt
+++ src/me/blzr/aaa/Main.kt
@@ -1,17 +1,16 @@
 package me.blzr.aaa
 
 fun main(args: Array<String>) {
-    val status = when {
-        args.isEmpty() -> {
-            println("No Args!!!")
-            0
-        }
-        args.size == 1 -> 1
-        args.size == 2 -> 2
-        else -> {
-            args.forEach { println(it) }
-            100
-        }
+    val status = when (args.size) {
+        0 -> getStatus(0, { println("No Args!!!") })
+        1 -> getStatus(1, {})
+        2 -> getStatus(2, {})
+        else -> getStatus(100, { args.forEach { println(it) } })
     }
     System.exit(status)
+}
+
+fun getStatus(status: Int, callback: () -> Unit): Int {
+    callback()
+    return status
 }
