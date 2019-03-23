25e452ffda40f9080cff9c09831300214382c490
Constantine Linnick <theaspect@gmail.com>
2019-03-23 04:04:23 +0000

    Add test counters

--- TEST.sh
+++ TEST.sh
@@ -1,11 +1,16 @@
 ./BUILD.sh
 
+COUNT_ALL=0
+COUNT_SUC=0
+
 echo
 echo No args
 ./RUN.sh
 RES=$?
-if [ $? -eq 0 ]; then
+let COUNT_ALL++
+if [ $RES -eq 0 ]; then
     echo Test passed
+    let "COUNT_SUC = $COUNT_SUC + 1"
 else
     echo Test failed. Expected 0 Actual $RES
 fi
@@ -14,8 +19,10 @@ echo
 echo 1 arg
 ./RUN.sh foo
 RES=$?
-if [ $? -eq 1 ]; then
+let COUNT_ALL++
+if [ $RES -eq 1 ]; then
     echo Test passed
+    let "COUNT_SUC = $COUNT_SUC + 1"
 else
     echo Test failed. Expected 1 Actual $RES
 fi
@@ -24,8 +31,10 @@ echo
 echo 2 args
 ./RUN.sh foo bar
 RES=$?
-if [ $? -eq 2 ]; then
+let COUNT_ALL++
+if [ $RES -eq 2 ]; then
     echo Test passed
+    let "COUNT_SUC = $COUNT_SUC + 1"
 else
     echo Test failed. Expected 2 Actual $RES
 fi
@@ -34,8 +43,19 @@ echo
 echo Many args
 ./RUN.sh foo bar baz "1 2 3"
 RES=$?
-if [ $? -eq 100 ]; then
+let COUNT_ALL++
+if [ $RES -eq 100 ]; then
     echo Test passed
+    let "COUNT_SUC = $COUNT_SUC + 1"
 else
     echo Test failed. Expected 100 Actual $RES
+fi
+
+echo
+if [ $COUNT_ALL -eq $COUNT_SUC ]; then
+    echo All $COUNT_ALL tests passed
+    exit 0
+else
+    echo $COUNT_SUC success tests out of $COUNT_ALL
+    exit 1
 fi
