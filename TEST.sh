1d1aa16808f87d4c36995291d3bb525a9adb2b72
Constantine Linnick <theaspect@gmail.com>
2019-03-23 04:31:22 +0000

    Tests refactoring

--- TEST.sh
+++ TEST.sh
@@ -3,53 +3,27 @@
 COUNT_ALL=0
 COUNT_SUC=0
 
-echo
-echo No args
-./RUN.sh
-RES=$?
-let COUNT_ALL++
-if [ $RES -eq 0 ]; then
-    echo Test passed
-    let "COUNT_SUC = $COUNT_SUC + 1"
-else
-    echo Test failed. Expected 0 Actual $RES
-fi
+function test {
+    MSG=$1; shift
+    EXPECTED=$1; shift
 
-echo
-echo 1 arg
-./RUN.sh foo
-RES=$?
-let COUNT_ALL++
-if [ $RES -eq 1 ]; then
-    echo Test passed
-    let "COUNT_SUC = $COUNT_SUC + 1"
-else
-    echo Test failed. Expected 1 Actual $RES
-fi
+    echo
+    echo $MSG
+    ./RUN.sh "$@"
+    RES=$?
+    let COUNT_ALL++
+    if [ $RES -eq $EXPECTED ]; then
+        echo Test passed
+        let "COUNT_SUC = $COUNT_SUC + 1"
+    else
+        echo Test failed. Expected $EXPECTED Actual $RES
+    fi
+}
 
-echo
-echo 2 args
-./RUN.sh foo bar
-RES=$?
-let COUNT_ALL++
-if [ $RES -eq 2 ]; then
-    echo Test passed
-    let "COUNT_SUC = $COUNT_SUC + 1"
-else
-    echo Test failed. Expected 2 Actual $RES
-fi
-
-echo
-echo Many args
-./RUN.sh foo bar baz "1 2 3"
-RES=$?
-let COUNT_ALL++
-if [ $RES -eq 100 ]; then
-    echo Test passed
-    let "COUNT_SUC = $COUNT_SUC + 1"
-else
-    echo Test failed. Expected 100 Actual $RES
-fi
+test "No args" 0
+test "1 arg" 1 foo
+test "2 args" 2 foo bar
+test "Many args" 100 foo bar baz "1 2 3"
 
 echo
 if [ $COUNT_ALL -eq $COUNT_SUC ]; then
